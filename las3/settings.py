b52e8a5df9c0d4f7173109df74e01a457323b186
Simeon Felis <simeonfelis@googlemail.com>
2012-04-27 16:13:57 +0200

    Added my home-computer for development

--- las3/settings.py
+++ las3/settings.py
@@ -6,4 +6,7 @@ if socket.gethostname() == 'rfhete470':
     from settings_production import *
 elif socket.gethostname() == 'pandora':
     from settings_dev_simeon import *
+elif socket.gethostname() == 'arpa':
+    from settings_dev_simeon import *
+
 
