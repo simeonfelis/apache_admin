60348bc8cb70a1cbaf22fa099d3616460000f833
Simeon Felis <simeonfelis@googlemail.com>
2012-05-04 16:38:33 +0200

    Made development host deliver static files

--- las3/settings_dev_simeon.py
+++ las3/settings_dev_simeon.py
@@ -50,13 +50,13 @@ USE_L10N = True
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
 # MEDIA_ROOT = ''
-MEDIA_ROOT = '/home/simeon/workspace/django/las3-django/las3/media/'
+MEDIA_ROOT = '/home/simeon/workspace/django/las3-django/las3-django/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
 # MEDIA_URL = ''
-MEDIA_URL = '/site_media/'
+MEDIA_URL = 'http://localhost/django/site_media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
